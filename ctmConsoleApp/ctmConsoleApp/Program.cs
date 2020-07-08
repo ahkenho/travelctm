355161f6100ad7b15c4d89a4f4af8c5a695ad898
Kenneth Ho <ahkenho@gmail.com>
2020-07-08 12:49:16 -0400

    initial commit from local

---
+++ ctmConsoleApp/ctmConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ctmConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
